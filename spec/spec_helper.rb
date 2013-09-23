5ec92d35fc49f21834bbca2e2680274ed58fa3f2
Leonardo Rodrigues de Mello <l@lmello.eu.org>
2013-09-23 18:33:39 -0300

    added spec helper and spec opts files.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'rspec'
+require 'mcollective'
+require 'mcollective/test'
+require 'rspec/mocks'
+require 'mocha'
+require 'tempfile'
+
+RSpec.configure do |config|
+  config.mock_with :mocha
+  config.include(MCollective::Test::Matchers)
+
+  config.before :each do
+    MCollective::PluginManager.clear
+  end
+end
